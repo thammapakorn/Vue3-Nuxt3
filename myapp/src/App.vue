daf7757cd3a5c6092e3f953a00d7e0ab0ed80736
Thammapakorn <121923286+thammapakorn@users.noreply.github.com>
2025-02-19 17:50:33 +0700

    แต่ง css ด้วย tailwind

--- myapp/src/App.vue
+++ myapp/src/App.vue
@@ -20,25 +20,28 @@ function addList() {
   }
 }
 
-function deleteList(index: number){
-    todoList.value.splice(index,1)
-  }
+function deleteList(index: number) {
+  todoList.value.splice(index, 1);
+}
 </script>
 
 <template>
-  <div>
-    <h1>Todo APP</h1>
-    <ul>
-      <li v-for="(todo, index) in todoList" :key="index">
-        <input v-model="todo.done" type="checkbox" />
-        {{ todo.text }}
-        <a href="#" @click="deleteList(index)">ลบ</a>
-      </li>
-    </ul>
-    <form @submit.prevent="addList">
-      <input v-model="todoInput" type="text" placeholder="เพิ่มรายการใหม่" />
-      <button type="submit">เพิ่มรายการ</button>
-    </form>
+  <div class="container mx-auto my-5">
+    <div class="rounded shadow-lg p-4 w-100 mx-auto">
+      <h1 class="text-3xl font-bold text-center mb-5 text-blue-600 dark:text-sky-400">Todo APP</h1>
+      <ul class="mb-3">
+        <li v-for="(todo, index) in todoList" :key="index" class="flex-warp space-x-2">
+          <input v-model="todo.done" type="checkbox" />
+          <span class="grow w-100" :class="{'line-through' : todo.done}">{{ todo.text }}</span>
+          <a href="#" class="underline text-blue-500" @click="deleteList(index)">ลบ</a>
+        </li>
+      </ul>
+      <form @submit.prevent="addList" class="flex">
+        <input class="px-2 py-1 border rounded focus:outline-blue-500" 
+        v-model="todoInput" type="text" placeholder="เพิ่มรายการใหม่" />
+        <button class="bg-blue-500 px-3 py-1 ml-10 rounded text-white" type="submit">เพิ่มรายการ</button>
+      </form>
+    </div>
   </div>
 </template>
 
