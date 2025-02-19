540add67b0f60d326dfde28cb477e262d3405502
Thammapakorn <121923286+thammapakorn@users.noreply.github.com>
2025-02-19 16:36:07 +0700

    ลองสร้าง todolist ด้วย vue

--- myapp/src/App.vue
+++ myapp/src/App.vue
@@ -1,30 +1,49 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
+
+interface TodoItem {
+  text: string;
+  done: boolean;
+}
+const todoInput = ref(""); //ใช้เก็บค่า input
+const todoList = ref<TodoItem[]>([]); // รายการ Todo
+
+function addList() {
+  if (todoInput.value.trim()) {
+    //ป้องกันการเพิ่มค่าว่าง
+
+    todoList.value.push({
+      text: todoInput.value,
+      done: false,
+    });
+    todoInput.value = ""; // รีเซ็ต imput
+  }
+}
+
+function deleteList(index: number){
+    todoList.value.splice(index,1)
+  }
 </script>
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1>Todo APP</h1>
+    <ul>
+      <li v-for="(todo, index) in todoList" :key="index">
+        <input v-model="todo.done" type="checkbox" />
+        {{ todo.text }}
+        <a href="#" @click="deleteList(index)">ลบ</a>
+      </li>
+    </ul>
+    <form @submit.prevent="addList">
+      <input v-model="todoInput" type="text" placeholder="เพิ่มรายการใหม่" />
+      <button type="submit">เพิ่มรายการ</button>
+    </form>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.done {
+  text-decoration: line-through;
 }
 </style>
